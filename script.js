ae8389a82046adc31716dc9fa482653142655f54
Norbert <norbert.p83@gmail.com>
2023-11-27 04:15:09 +0000

    form torles modositas

--- script.js
+++ script.js
@@ -8,21 +8,6 @@
 //             window.scrollTo({ top: 0, behavior: 'smooth' });
 //         });
 //     }
-
-
-//     window.onload = function() {
-//         // Késleltetés beállítása a képek beúsztatásához
-//         const slideInDelay = 400; // 400 milliszekundum késleltetés
-    
-//         const leftImage = document.querySelector('.images-container img:first-child');
-//         const rightImage = document.querySelector('.images-container img:last-child');
-    
-//         if (leftImage && rightImage) {
-//             leftImage.style.animation = `slideInFromLeft 6s ease-out ${slideInDelay}ms forwards`;
-//             rightImage.style.animation = `slideInFromRight 6s ease-out ${slideInDelay}ms forwards`;
-//         }
-//     };
-
 // });
 
 window.onload = function() {
--- script3.js
+++ script3.js
@@ -41,16 +41,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Késleltetés az űrlapmezők törléséhez
-    setTimeout(function() {
-        var form = document.getElementById('myForm');
-        if (form) {
-            form.reset(); // Törli az űrlap mezőit
-        }
-    }, 1);
-});
-
 
 // A meglévő setTimeout függvényen belül
 setTimeout(function() {
@@ -67,68 +57,13 @@ setTimeout(function() {
 
 
 
-[
-            '/kepek/Projects Gallery/Back & Side Extension/1.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/2.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/3.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/4.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/5.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/6.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/7.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/8.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/9.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/10.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/11.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/12.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/13.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/14.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/15.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/16.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/17.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/18.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/19.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/20.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/21.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/22.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/23.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/24.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/25.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/26.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/27.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/28.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/29.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/30.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/31.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/32.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/33.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/34.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/35.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/36.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/37.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/38.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/39.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/40.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/41.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/42.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/43.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/44.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/45.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/46.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/47.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/48.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/49.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/50.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/51.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/52.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/53.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/54.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/55.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/56.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/57.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/58.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/59.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/60.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/61.jpg',
-            '/kepek/Projects Gallery/Back & Side Extension/62.jpg',
 
-        ]
+document.addEventListener('DOMContentLoaded', function() {
+    // Késleltetés az űrlapmezők törléséhez
+    setTimeout(function() {
+        var form = document.getElementById('myForm');
+        if (form) {
+            form.reset(); // Törli az űrlap mezőit
+        }
+    }, 10);
+});
