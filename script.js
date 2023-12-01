ac46331fa2422b3e4a30bae01954074a5b7503b0
Norbert <norbert.p83@gmail.com>
2023-12-01 13:02:29 +0000

    kesleltetes 400 rol 100ms re allitva

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ window.onload = function() {
     // Smooth scroll és egyéb kódok...
 
     // Késleltetés és animáció beállítása a képek beúsztatásához
-    const slideInDelay = 1; // 1 milliszekundum késleltetés
+    const slideInDelay = 100; // 1 milliszekundum késleltetés
     const leftImage = document.querySelector('.images-container img:first-child');
     const rightImage = document.querySelector('.images-container img:last-child');
     if (leftImage && rightImage) {
-        leftImage.style.animation = `slideInFromLeft 3s ease-out ${slideInDelay}ms forwards`;
-        rightImage.style.animation = `slideInFromRight 3s ease-out ${slideInDelay}ms forwards`;
+        leftImage.style.animation = `slideInFromLeft 1s ease-out ${slideInDelay}ms forwards`;
+        rightImage.style.animation = `slideInFromRight 1s ease-out ${slideInDelay}ms forwards`;
     }
 };
 
