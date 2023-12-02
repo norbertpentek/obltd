fe68404c78f10b418a51122ddb9d3ac708c703b9
Norbert <norbert.p83@gmail.com>
2023-12-02 03:07:36 +0000

    Facebook icon fel

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-window.onload = function() {
-    // Smooth scroll és egyéb kódok...
+// window.onload = function() {
+//     // Smooth scroll és egyéb kódok...
 
-    // Késleltetés és animáció beállítása a képek beúsztatásához
-    const slideInDelay = 100; // 1 milliszekundum késleltetés
-    const leftImage = document.querySelector('.images-container img:first-child');
-    const rightImage = document.querySelector('.images-container img:last-child');
-    if (leftImage && rightImage) {
-        leftImage.style.animation = `slideInFromLeft 2s ease-out ${slideInDelay}ms forwards`;
-        rightImage.style.animation = `slideInFromRight 2s ease-out ${slideInDelay}ms forwards`;
-    }
-};
+//     // Késleltetés és animáció beállítása a képek beúsztatásához
+//     const slideInDelay = 100; // 1 milliszekundum késleltetés
+//     const leftImage = document.querySelector('.images-container img:first-child');
+//     const rightImage = document.querySelector('.images-container img:last-child');
+//     if (leftImage && rightImage) {
+//         leftImage.style.animation = `slideInFromLeft 2s ease-out ${slideInDelay}ms forwards`;
+//         rightImage.style.animation = `slideInFromRight 2s ease-out ${slideInDelay}ms forwards`;
+//     }
+// };
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js')
-        .then(reg => {
-          console.log('Service worker registered.', reg);
-        });
-    });
-  }
+// if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', () => {
+//       navigator.serviceWorker.register('/service-worker.js')
+//         .then(reg => {
+//           console.log('Service worker registered.', reg);
+//         });
+//     });
+//   }
