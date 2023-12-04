eaea9c48b6bd8d0c65b1e50fda5cb9914bcfa06c
Norbert <norbert.p83@gmail.com>
2023-12-04 10:13:28 +0000

    megjelenesi idok csokkentese js3.js ideiglenes kikommenteles

--- service-worker.js
+++ service-worker.js
@@ -22,6 +22,26 @@ const STATIC_ASSETS = [
   '/kepek/Projects Gallery/Back & Side Extension3/6.webp',
   '/kepek/Projects Gallery/Back & Side Extension3/7.webp',
   '/kepek/Projects Gallery/Back & Side Extension3/8.webp',
+  '/kepek/Projects Gallery/Back & Side Extension/1.webp',
+  '/kepek/Projects Gallery/Back & Side Extension/2.webp',
+  '/kepek/Projects Gallery/Back & Side Extension/3.webp',
+  '/kepek/Projects Gallery/Back & Side Extension/4.webp',
+  '/kepek/Projects Gallery/Back & Side Extension/5.webp',
+  '/kepek/Projects Gallery/Back & Side Extension/6.webp',
+  '/kepek/Projects Gallery/Back & Side Extension/7.webp',
+  '/kepek/Projects Gallery/Back & Side Extension/8.webp',
+  '/kepek/Projects Gallery/Back & Side Extension/9.webp',
+  '/kepek/Projects Gallery/Back & Side Extension/10.webp',
+  '/kepek/Projects Gallery/Back & Side Extension/11.webp',
+  '/kepek/Projects Gallery/Back & Side Extension/12.webp',
+  '/kepek/Projects Gallery/Back & Side Extension/13.webp',
+  '/kepek/Projects Gallery/Back & Side Extension/14.webp',
+  '/kepek/Projects Gallery/Back & Side Extension/15.webp',
+  '/kepek/Projects Gallery/Back & Side Extension/16.webp',
+  '/kepek/Projects Gallery/Back & Side Extension/17.webp',
+  '/kepek/Projects Gallery/Back & Side Extension/18.webp',
+  '/kepek/Projects Gallery/Back & Side Extension/19.webp',
+  '/kepek/Projects Gallery/Back & Side Extension/20.webp',
   // Include additional static assets
 ];
 
@@ -32,7 +52,6 @@ self.addEventListener('install', event => {
       .then(cache => cache.addAll(STATIC_ASSETS))
   );
 });
-
 // Activate event - cleaning up old caches
 self.addEventListener('activate', event => {
   event.waitUntil(
@@ -46,7 +65,6 @@ self.addEventListener('activate', event => {
       ))
   );
 });
-
 // Fetch event - serving up the static files (if in cache)
 self.addEventListener('fetch', event => {
   event.respondWith(
