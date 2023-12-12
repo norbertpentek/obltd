d73e13847eeab4b76a24e21960861d5d697ac513
Norbert <norbert.p83@gmail.com>
2023-12-12 14:49:50 +0000

    kisebb nev javitasok2  v98

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-cache-v1.97';
+const CACHE_NAME = 'static-cache-v1.98';
 const STATIC_ASSETS = [
   // Add paths to all of your static files here
   '/video/mesure5.mp4',
@@ -84,4 +84,3 @@ self.addEventListener('fetch', function(event) {
     })
   );
 });
-
