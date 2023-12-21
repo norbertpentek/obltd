922d23601b1b4f9625008d2907bf22d9fe0ad34b
Norbert <norbert.p83@gmail.com>
2023-12-21 19:29:00 +0000

    kuldesi torlesi problema jav proba

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-cache-v8.8';
+const CACHE_NAME = 'static-cache-v8.9';
 const STATIC_ASSETS = [
   // Add paths to all of your static files here
      '/css/syliesgoko.css',
