c653ff65dcd4bfd40f9a1e315c378184825888a1
Norbert <norbert.p83@gmail.com>
2024-01-22 19:43:24 +0000

    szoveg szurkere1

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-cache-v9.16';
+const CACHE_NAME = 'static-cache-v9.17';
 const STATIC_ASSETS = [
   // Add paths to all of your static files here
      '/css/syliesgoko.css',
