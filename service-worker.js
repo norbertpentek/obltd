341ac0113106e5154ba01cc98a3291169a8663b0
Norbert <norbert.p83@gmail.com>
2024-01-22 18:17:24 +0000

    social media ikonok meretre szabasa1

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-cache-v9.14';
+const CACHE_NAME = 'static-cache-v9.15';
 const STATIC_ASSETS = [
   // Add paths to all of your static files here
      '/css/syliesgoko.css',
