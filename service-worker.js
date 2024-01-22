f8f3abf6c8dfd297e7a40fc37d1b934c562ad361
Norbert <norbert.p83@gmail.com>
2024-01-22 16:04:44 +0000

    nav hatter nagyobb nav menu font size

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-cache-v9.8';
+const CACHE_NAME = 'static-cache-v9.10';
 const STATIC_ASSETS = [
   // Add paths to all of your static files here
      '/css/syliesgoko.css',
