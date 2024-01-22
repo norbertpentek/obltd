e22abbefb193327b3f7619e4239fd47ed2c01e50
Norbert <norbert.p83@gmail.com>
2024-01-22 16:35:02 +0000

    contact us hatter

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-cache-v9.11';
+const CACHE_NAME = 'static-cache-v9.12';
 const STATIC_ASSETS = [
   // Add paths to all of your static files here
      '/css/syliesgoko.css',
