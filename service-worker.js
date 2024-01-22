edca360d14b02eb214730012480c844ec4198800
Norbert <norbert.p83@gmail.com>
2024-01-22 18:22:13 +0000

    social media ikonok margin 15 re 3 rol allitva

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-cache-v9.15';
+const CACHE_NAME = 'static-cache-v9.16';
 const STATIC_ASSETS = [
   // Add paths to all of your static files here
      '/css/syliesgoko.css',
