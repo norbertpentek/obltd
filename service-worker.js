12ea9a3611ae11b369b60489017313c9f07d1c31
Norbert <norbert.p83@gmail.com>
2024-01-23 10:18:21 +0000

    s w uj

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-cache-v9.17';
+const CACHE_NAME = 'static-cache-v9.18';
 const STATIC_ASSETS = [
   // Add paths to all of your static files here
      '/css/syliesgoko.css',
