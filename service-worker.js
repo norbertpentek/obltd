7ebdd850ef832c8fa140068c83c745b76082f4d4
Norbert <norbert.p83@gmail.com>
2024-01-23 12:12:36 +0000

    logo jav alul raszerkesztve Service LTD 1.1

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-cache-v9.21';
+const CACHE_NAME = 'static-cache-v9.22';
 const STATIC_ASSETS = [
   // Add paths to all of your static files here
      '/css/syliesgoko.css',
