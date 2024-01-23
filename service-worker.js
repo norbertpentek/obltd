494a0d591c2e7278c28d820752d52faed9fdb6be
Norbert <norbert.p83@gmail.com>
2024-01-23 12:55:04 +0000

    privacy policy lathatova valt

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-cache-v9.24';
+const CACHE_NAME = 'static-cache-v9.25';
 const STATIC_ASSETS = [
   // Add paths to all of your static files here
      '/css/syliesgoko.css',
