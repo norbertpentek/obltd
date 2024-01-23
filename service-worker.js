d36071f9fdd8bc061d7964082a317a94599d8f3c
Norbert <norbert.p83@gmail.com>
2024-01-23 13:19:36 +0000

    privacy policy szoveg  formazas upgrade

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'static-cache-v9.26';
+const CACHE_NAME = 'static-cache-v9.27';
 const STATIC_ASSETS = [
   // Add paths to all of your static files here
      '/css/syliesgoko.css',
