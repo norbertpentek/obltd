dd32393654dd4a1223ce5326f7c327e3a96f377b
Norbert Pentek <norbert.p83@gmail.com>
2024-11-30 08:02:19 +0000

    finomhangolva a kezdo kepernyo

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "static-cache-v9.60";
+const CACHE_NAME = "static-cache-v9.62";
 const STATIC_ASSETS = [
   // Add paths to all of your static files here
   //"/css/syliesgoko.css",
