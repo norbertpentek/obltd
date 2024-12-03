4c780283bfc8fe5753bef1b4479b23735c9fa2ed
Norbert Pentek <norbert.p83@gmail.com>
2024-12-03 06:17:40 +0000

    veglege kiveve pg

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "static-cache-v9.94";
+const CACHE_NAME = "static-cache-v9.95";
 const STATIC_ASSETS = [
   // Add paths to all of your static files here
   //"/css/syliesgoko.css",
