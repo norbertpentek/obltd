ed47e5075eba76b583a238fb5e321f03845fb8f8
Norbert Pentek <norbert.p83@gmail.com>
2024-12-03 04:15:32 +0000

    sotet hatter video

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "static-cache-v9.92";
+const CACHE_NAME = "static-cache-v9.93";
 const STATIC_ASSETS = [
   // Add paths to all of your static files here
   //"/css/syliesgoko.css",
