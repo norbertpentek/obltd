643b1699dbf027b13f2a4a2cc62eb8991b54dc1b
Norbert Pentek <norbert.p83@gmail.com>
2024-12-03 07:55:26 +0000

    sky kicsinyitve

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "static-cache-v10.1";
+const CACHE_NAME = "static-cache-v10.2";
 const STATIC_ASSETS = [
   // Add paths to all of your static files here
   "/css/syliesgoko.css",
