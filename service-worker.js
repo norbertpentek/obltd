3f5034cbd2637b831b3f305afe35095d3992f2eb
Norbert Pentek <norbert.p83@gmail.com>
2024-12-11 10:55:58 +0000

    keszen van

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "static-cache-v10.23";
+const CACHE_NAME = "static-cache-v10.24";
 const STATIC_ASSETS = [
   // Add paths to all of your static files here
   "/css/syliesgoko.css",
